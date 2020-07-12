e5c05270225ad2fa01e34f80015a017301ee5676
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2020-07-11 22:22:38 -0700

    Remove usage of uninitialized variables

--- rply.c
+++ rply.c
@@ -372,14 +372,13 @@ p_ply ply_open(const char *name, p_ply_error_cb error_cb,
 
 p_ply ply_open_from_file(FILE *fp, p_ply_error_cb error_cb,
         long idata, void *pdata) {
-    p_ply ply;
     if (error_cb == NULL) error_cb = ply_error_cb;
     assert(fp);
     if (!ply_type_check()) {
-        error_cb(ply, "Incompatible type system");
+        error_cb(NULL, "Incompatible type system");
         return NULL;
     }
-    ply = ply_alloc();
+    p_ply ply = ply_alloc();
     if (!ply) {
         error_cb(NULL, "Out of memory");
         return NULL;
@@ -475,13 +474,12 @@ p_ply ply_create(const char *name, e_ply_storage_mode storage_mode,
 
 p_ply ply_create_to_file(FILE *fp, e_ply_storage_mode storage_mode,
         p_ply_error_cb error_cb, long idata, void *pdata) {
-    p_ply ply;
     assert(fp && storage_mode <= PLY_DEFAULT);
     if (!ply_type_check()) {
-        error_cb(ply, "Incompatible type system");
+        error_cb(NULL, "Incompatible type system");
         return NULL;
     }
-    ply = ply_alloc();
+    p_ply ply = ply_alloc();
     if (!ply) {
         error_cb(NULL, "Out of memory");
         return NULL;
